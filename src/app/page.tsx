25e6bae1c62633ae48d5d09c8306acd9c23312d1
kiudev <dani.97.saavedra@gmail.com>
2024-04-19 20:14:43 +0200

    create project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,177 @@
+"use client";
 import Image from "next/image";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+   Carousel,
+   CarouselContent,
+   CarouselItem,
+   CarouselPrevious,
+   CarouselNext,
+} from "@/components/ui/carousel";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import {
+   NavigationMenu,
+   NavigationMenuList,
+   NavigationMenuItem,
+   NavigationMenuTrigger,
+   NavigationMenuContent,
+} from "@/components/ui/navigation-menu";
+import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   const [item, setItem] = useState<{ id: number; posterPath: string }[]>([]);
+   const [loading, setLoading] = useState(true);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+   const getItems = () => {
+      axios
+         .get(
+            `https://api.themoviedb.org/3/discover/movie?api_key=d7d9147d7ae46dbada53ea4a821b8ded`
+         )
+         .then(response => {
+            const data = response.data.results.map(
+               (movie: {
+                  id: number;
+                  title: string;
+                  overview: string;
+                  poster_path: string;
+               }) => {
+                  const { id, title, overview, poster_path } = movie;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                  return {
+                     id,
+                     title,
+                     overview,
+                     posterPath: poster_path,
+                  };
+               }
+            );
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            setItem(data);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            setTimeout(() => {
+               setLoading(false);
+            }, 2000);
+         });
+   };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+   useEffect(() => {
+      getItems();
+   }, []);
+
+   const streamingLogos = [
+      {
+         id: 0,
+         name: "Netflix",
+         image: "/netflix.svg",
+         link: "Top 10 Netflix",
+         new: "Newest",
+      },
+      {
+         id: 1,
+         name: "HBO Max",
+         image: "/hbo.svg",
+         link: "Top 10 HBO",
+      },
+      {
+         id: 2,
+         name: "Disney Plus",
+         image: "/disney.svg",
+         link: "Top 10 Disney",
+      },
+      {
+         id: 3,
+         name: "Amazon Prime Video",
+         image: "/prime.svg",
+         link: "Top 10 Prime Video",
+      },
+   ];
+
+   return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-gradient-to-b from-blue-800 from-50% to-blue-900 to-200%">
+         {/* <Carousel className="w-80">
+            <CarouselContent className="w-[30em]">
+               {item.map(data => (
+                  <CarouselItem key={data.id} className="basis-1/8 w-auto h-auto">
+                     <Image
+                        alt=""
+                        src={`https://image.tmdb.org/t/p/w342${data.posterPath}`}
+                        width={200}
+                        height={200}
+                     />
+                  </CarouselItem>
+               ))}
+            </CarouselContent>
+            <CarouselPrevious className="text-blue-500"/>
+            <CarouselNext className="text-blue-500" />
+         </Carousel> */}
+         <NavigationMenu>
+            <NavigationMenuList>
+               {streamingLogos.map(logo => (
+                  <NavigationMenuItem key={logo.id} className="flex flex-row">
+                     <NavigationMenuTrigger className="text-blue-600 hover:text-blue-600">
+                        <Image
+                           src={logo.image}
+                           width={70}
+                           height={70}
+                           alt={logo.name}
+                        />
+                     </NavigationMenuTrigger>
+                     <NavigationMenuContent className="bg-blue-900 animate-in border border-blue-800">
+                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                           <li className="row-span-3">
+                              <NavigationMenuLink asChild>
+                                 <a
+                                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                    href="/"
+                                 >
+                                    <div className="mb-2 mt-4 text-lg font-medium text-blue-800">
+                                       {logo.link}
+                                    </div>
+                                    <p className="text-sm leading-tight text-muted-foreground text-blue-600">
+                                       Beautifully designed components that you
+                                       can copy and paste into your apps.
+                                       Accessible. Customizable. Open Source.
+                                    </p>
+                                 </a>
+                              </NavigationMenuLink>
+                           </li>
+                           <li>
+                              <NavigationMenuLink>
+                                 <a href="#">{logo.new}</a>
+                              </NavigationMenuLink>
+                           </li>
+                        </ul>
+                     </NavigationMenuContent>
+                  </NavigationMenuItem>
+               ))}
+            </NavigationMenuList>
+         </NavigationMenu>
+
+         <Input
+            className="bg-blue-600 bg-opacity-30 rounded-[5px] border-none mt-10 px-5 py-6 w-96 text-center text-blue-600 text-lg"
+            placeholder="Search"
+         />
+
+         {loading ? (
+            <div>Loading...</div>
+         ) : (
+            <section className="flex flex-wrap mt-10 justify-center flex-row w-[90vw] xl:w-[1200px] gap-3">
+               {item.map(data => (
+                  <Card className="border-transparent shadow-xl" key={data.id}>
+                     <Image
+                        className="rounded-xl w-[40vw] sm:w-[25vw] md:w-[20vw] lg:w-[200px]"
+                        alt=""
+                        src={`https://image.tmdb.org/t/p/w342${data.posterPath}`}
+                        width={200}
+                        height={200}
+                     />
+                  </Card>
+               ))}
+            </section>
+         )}
+      </main>
+   );
 }
