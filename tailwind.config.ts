7a228afc628551b505f8bf17b768a5a71656aee1
kiudev <dani.97.saavedra@gmail.com>
2024-05-13 18:42:42 +0200

    add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,14 +71,23 @@ const config = {
                from: { height: "var(--radix-accordion-content-height)" },
                to: { height: "0" },
             },
+            "slide-right": {
+               "0%": { transform: "translateX(100%)" },
+               "100%": { transform: "translateX(0)" },
+            },
          },
          animation: {
             "accordion-down": "accordion-down 0.2s ease-out",
             "accordion-up": "accordion-up 0.2s ease-out",
+            "slide-right": "slide-right 0.5s ease-in-out",
          },
       },
    },
-   plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar"), require('tailwindcss-animated')]
+   plugins: [
+      require("tailwindcss-animate"),
+      require("tailwind-scrollbar"),
+      require("tailwindcss-animated"),
+   ],
 } satisfies Config;
 
 export default config;
